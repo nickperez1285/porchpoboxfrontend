d54d34e7ac0101e3c1c1e0bd42198447375370cf
nick perez <nickperez1285@gmail.com>
2025-12-14 23:18:44 -0800

    ms

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import API_BASE_URL from "./config/api";
+import API_BASE_URL from "porchpoboxfrontend/src/config/api.js";
 
 // import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is included
 
