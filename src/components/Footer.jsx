ef49d1b9910b74d14bca3e9f4d4e5f6745333614
Nick <nickperez1285@gmail.com>
2026-08-13 18:55:11 -0700

    fb link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,6 +22,24 @@ const Footer = () => {
               <span>Porch P.O. Box</span>
             </Link>
             <span>Secure. Local. Convenient.</span>
+            <a
+              href="https://www.facebook.com/profile.php?id=61593214381152"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Porch P.O. Box on Facebook"
+              className="mp-footer-social"
+            >
+              <svg
+                viewBox="0 0 24 24"
+                width="20"
+                height="20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047v-2.66c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.971H15.83c-1.491 0-1.956.93-1.956 1.886v2.264h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z" />
+              </svg>
+              <span>Facebook</span>
+            </a>
           </div>
 
           <div>
